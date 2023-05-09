3a70406bb92a87d592f3630e57f6a0ec8b402b30
OphelieThomas33 <ophelie_thomas@hotmail.com>
2023-05-09 10:56:37 +0200

    creation of template

--- blogs/urls.py
+++ blogs/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "blogs"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<int:blog_id>/", views.detail, name="detail"),
     
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from .models import Blog
+from django.template import loader
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello World !')
+    all_blogs = Blog.objects.all()
+    template = loader.get_template("blogs/index.html")
+    context = {
+        "all_blogs": all_blogs,
+    }
+    return HttpResponse(template.render(context, request))
+
+def detail(request, blog_id):
+    try:
+        blog = Blog.objects.get(pk=blog_id)
+    except Blog.DoesNotExist:
+        raise Http404("Blog does not exist")
+    return render(request, "blogs/detail.html", {"blog": blog})
